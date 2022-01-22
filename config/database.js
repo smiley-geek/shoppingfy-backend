218429e550ada5b9994dc09139050e9ee4c66ff9
smiley-geek <kariukigeorge2030@gmail.com>
2022-01-22 13:58:21 +0300

    *

--- config/database.js
+++ config/database.js
@@ -15,5 +15,13 @@ const connection = mongoose.connect(DB_STRING, options, (err) => {
   if (err) return console.log(err);
   console.log("connected to db");
 });
+/*GOOGLE_CLIENT_ID=915138254141-plku2l4sl310rfbsng8gcb5epo0i3pjg.apps.googleusercontent.com
+GOOGLE_CLIENT_SECRET=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
+DB_STRING=mongodb://localhost:27017/shoppingfy
+COOKIE_SECRET=c7d67d15473641dbc96751b0a233f42f
+SMTP_SERVER=smtp-relay.sendinblue.com
+SMTP_PORT=587
+SMTP_LOGIN=kariukigeorge2030@gmail.com
+SMTP_PASSWORD=Mvm36NEwTDapK8zc*/
 
 module.exports = connection;
